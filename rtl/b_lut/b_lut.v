1201564f41792810382c2c59f7a96c29ed8c1f41
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-05-31 15:25:19 +0100

    Don't be clever - b_lut area improvments
    - Specifying b_lut naievely gives better synthesis results.
    
     On branch master
     Your branch is ahead of 'origin/master' by 3 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   rtl/b_lut/b_lut.v

--- rtl/b_lut/b_lut.v
+++ rtl/b_lut/b_lut.v
@@ -29,14 +29,7 @@ end endgenerate
 genvar j;
 generate for (j = 0; j < 8; j = j + 1) begin
 
-    wire [3:0] lut_in = crs1[4*j+3:4*j];
-
-    wire [31:0] l1 = lut_in[3] ? lut_con[63:32] : lut_con[31:0];
-    wire [15:0] l2 = lut_in[2] ? l1[31:16] : l1[15:0];
-    wire [ 7:0] l3 = lut_in[1] ? l2[15: 8] : l2[ 7:0];
-    wire [ 3:0] l4 = lut_in[0] ? l3[ 7: 4] : l3[ 3:0];
-
-    assign result[4*j+3:4*j] = l4;
+    assign result[4*j+3:4*j] = lut_arr[crs1[4*j+3:4*j]];
 
 end endgenerate
 
